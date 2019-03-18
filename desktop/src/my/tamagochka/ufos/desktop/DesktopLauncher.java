7955efeb3b0dface8b018e4b243b588bdc2aa05b
tamagochka <tamagochka@gmail.com>
2019-03-18 16:14:53 +0300

    Initial commit.

---
+++ desktop/src/my/tamagochka/ufos/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package my.tamagochka.ufos.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import my.tamagochka.ufos.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
